7382f910aae6373ccb2b301addacd55c232ec905
raiz <furlan.raizigue@gmail.com>
2022-04-05 09:59:21 -0400

    clone

---
+++ Views/EscolaFormsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Escola_atv.Views
+{
+    /// <summary>
+    /// Lógica interna para EscolaFormsWindow.xaml
+    /// </summary>
+    public partial class EscolaFormsWindow : Window
+    {
+        public EscolaFormsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
